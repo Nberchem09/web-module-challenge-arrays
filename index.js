6777da873bd99ab387b5daaed7b91f0bd32e8c5b
Nick Berchem <nick.berchem09@gmail.com>
2020-09-09 19:30:33 -0500

    Wrote JS for task 3 and tested successfully in console. All tasks will now include the console log from task one to verify flavors are or are not equal to 31 as the task requires

--- index.js
+++ index.js
@@ -79,10 +79,12 @@ Your function should remove a flavor from the end of the array and console.log t
 
 For example removeLastFlavor(originalFlavors) would return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla"]*/ 
 
-// function removeLastFlavor(originalFlavors){
-//     return (originalFlavors.pop());
-// }
-// console.log (originalFlavors)
+function removeLastFlavor(originalFlavors){
+    return (originalFlavors.pop());
+}
+removeLastFlavor(originalFlavors)
+console.log (originalFlavors)
+console.log(is31Flavors(originalFlavors))
 
 /* Task 4: Write a function that returns a flavor at a given index in the array.
 
